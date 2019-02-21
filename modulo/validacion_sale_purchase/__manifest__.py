890625adc8e95b23cde3d7642b9620a4fed1328e
Gil <acevedo.4297@gmail.com>
2019-02-21 09:33:33 -0600

    Informacion de desarrollo

--- modulo/validacion_sale_purchase/__manifest__.py
+++ modulo/validacion_sale_purchase/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "validacion",
+    'name': "blockade_sale_purchase",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -10,7 +10,8 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
+    'author': "xmarts",
+    'contributors': "Gilberto Santiago Acevedo - Luis Angel Gonzalez"
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
--- modulo/validacion_sale_purchase/models/models.py
+++ modulo/validacion_sale_purchase/models/models.py
@@ -4,10 +4,3 @@ from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
 from odoo.exceptions import UserError
 
-#class ValidationPurchase(models.Model):
-
-	#_inherit = 'purchase.order'
-
-	#@api.one
-	#def button_confirm(self):
-	#	raise ValidationError('Bien...')
